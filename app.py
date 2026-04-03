e9fdcc81696d32692f0bc73838ce70fafd3df17e
MySuccessGate <ibolaoye93@gmail.com>
2026-04-03 18:54:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.set_page_config(page_title="Test")
 st.title("✅ Test de déploiement")
-st.write("Si vous voyez ce message, Streamlit Cloud fonctionne correctement.")﻿
+st.write("si vous voyez ce message, streamlit cloud fonctionne correctement.")
